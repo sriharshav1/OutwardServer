26f3f761814f73fd082be6083f866035370e9f6a
sriharshav1 <sriharsha.vanga@sjsu.edu>
2019-03-07 22:12:09 -0800

    comments added

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+// Setting up the cors policy the application is set currently to accept all the endpoints
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
@@ -14,30 +15,26 @@ app.use((req, res, next) => {
   next();
 });
 var userCount=0;
+// Handling the dynamic port condition
 const PORT = process.env.PORT || 3001;
-
+// Home route to calculate all the visitors in the server running state
 app.get('/', function (req, res) {
  
    
    res.json({ result: userCount++ });
   
   });
-
+// route to calculate and send the addition request
 app.get('/add', function (req, res) {
-console.log("in post");
-
-//let searchParams = new URLSearchParams(url.search);
-//console.log(searchParams.get('c')); 
 
  let value1 = req.query.value1;
  let value2 = req.query.value2;
- //searchParams.get('value1');
- //let value2 = searchParams.get('value2');
+
  console.log(Number(value1) + Number(value2));
  res.json({ result: Number(value1) + Number(value2) });
 
 });
-
+// route to calculate and send the substraction request
 app.get('/sub', function (req, res) {
   console.log("in post");
    let value1 = req.query.value1;
@@ -45,6 +42,7 @@ app.get('/sub', function (req, res) {
    res.json({ result: Number(value1) - Number(value2) });
   
   });
+  // route to calculate and send the multiplication request
   app.get('/mul', function (req, res) {
      let value1 = req.query.value1;
      let value2 = req.query.value2;
@@ -53,6 +51,7 @@ app.get('/sub', function (req, res) {
      res.json({ result: Number(value1) * Number(value2) });
     
     });
+   // route to calculate and send the division request
     app.get('/div', function (req, res) {
        let value1 = req.query.value1;
        let value2 = req.query.value2;
@@ -61,18 +60,6 @@ app.get('/sub', function (req, res) {
        res.json({ result: result });
       
       });
-app.get('/', (req, res) => {
-    userCount++;
-    //res.writeHead(200, { 'Content-Type': 'text/plain' });
-   // res.write('Hello!\n');
-    // res.write('We have had ' + userCount + ' visits!\n');
-    // res.end();
-
-  res.send('We have had ' + userCount + ' visits!\n')
-});
-app.get('/name', (req, res) => {
-  res.send('hi john')
-});
 
 app.listen(PORT, () => {
   console.log(`Our app is running on port ${ PORT }`);
